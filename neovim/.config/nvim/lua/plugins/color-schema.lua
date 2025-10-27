39ee5189984a8c7fa73c5af22730b8a6ad9d2eff
hongarc <hongtu@everfit.io>
2025-10-27 21:51:34 +0700

    feat(nvim): improve color for float

--- neovim/.config/nvim/lua/plugins/color-schema.lua
+++ neovim/.config/nvim/lua/plugins/color-schema.lua
@@ -7,21 +7,11 @@ return {
       flavour = "macchiato",
       transparent_background = true,
       show_end_of_buffer = true,
-      styles = {
-        comments = { "italic" }, -- Friendly, subtle
-        conditionals = { "italic" }, -- Stand out logic flow
-        loops = { "italic" }, -- Like conditionals for consistency
-        functions = { "italic", "bold" }, -- Emphasize function names
-        keywords = { "bold" }, -- Core language constructs
-        strings = {}, -- Leave clean, easy to read
-        variables = {}, -- Neutral, to avoid visual clutter
-        numbers = { "italic" }, -- Subtle styling
-        booleans = { "bold", "underline" }, -- Emphasize true/false clearly
-        properties = { "italic" }, -- Helps distinguish object properties
-        types = { "bold" }, -- Strong presence for types (TypeScript, etc.)
-        operators = {}, -- Leave neutral
-        -- miscs = {}, -- Can be set if you want to tweak `@constant`, `@field`, etc.
+      float = {
+        transparent = true,
+        solid = false,
       },
+      auto_integrations = true,
     },
   },
   {
